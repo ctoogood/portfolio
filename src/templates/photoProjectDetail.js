f0a46417cdf029d4988f79dcdab2dc5498eaeac6
ctoogood <calumtoogood@gmail.com>
2020-05-26 19:46:45 +0100

    edited lightbox

--- src/templates/photoProjectDetail.js
+++ src/templates/photoProjectDetail.js
@@ -7,11 +7,19 @@ import "./projectDetail.scss"
 
 const Post = ({ data: { project }, errors }) => {
   const options = {
-    showThumbnails: false,
-    captionFontWeight: "300",
-    autoplaySpeed: 0,
-    showDownloadButton: false,
-    enablePanzoom: false,
+    settings: {
+      autoplaySpeed: 0,
+      enablePanzoom: false,
+    },
+    buttons: {
+      showDownloadButton: false,
+    },
+    thumbnails: {
+      showThumbnails: false,
+    },
+    captions: {
+      captionFontWeight: "300",
+    },
   }
 
   return (
